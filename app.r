87fef9e14bd778ad6206639875e4c4059898aea0
eugenekuloba <eugene.kuloba@student.moringaschool.com>
2023-08-23 15:10:43 +0300

    upates

--- app.r
+++ app.r
@@ -1,14 +1,41 @@
 # load necessary libraries
 library(dplyr)
-library(sf)
-library(readxl)
+library(ggplot2)
+library(sf) 
+library(plotly)
+library(htmlwidgets)
+library(shiny)
 
-# read xlsx file
-data <- read_excel("cleaned_xlsx_data/cleaned_data.xlsx")
-View(data)
- 
-# Create an sf object with point geometries from lat and long columns
-sf_data <- st_as_sf(data, coords = c("longitude", "latitude"), crs = 4326)
+# load dataset
+data <- sf::st_read("file.geojson")
 
-# Visualize data
-plot(sf_data)
+# Define the UI for the Shiny app
+ui <- fluidPage(
+  titlePanel("Interactive Map with Shiny"),
+  # Place your interactive map here
+  plotlyOutput("plot")
+)
+
+# Define the server logic for the Shiny app
+server <- function(input, output) {
+  # Load your spatial data
+  data <- st_read("file.geojson")
+  
+  # Create ggplot with hover text
+  gg <- ggplot() +
+    geom_sf(data = data, aes(fill = beneficiary_count,
+                             text = paste("County:", county, "<br>Constituency:", constituency,"<br>Population Count:",population_count))) +
+    scale_fill_gradientn(colors = rev(heat.colors(num_colors))) +
+    labs(title = "Beneficiary Counts Map")
+  
+  # Convert to plotly object
+  plotly_map <- ggplotly(gg)
+  
+  # Display interactive map
+  output$plot <- renderPlotly({
+    plotly_map
+  })
+}
+
+# Run the Shiny app
+shinyApp(ui, server)
